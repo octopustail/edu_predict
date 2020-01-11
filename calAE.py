e381ab44ace11aa35b4580727e919218e8bf7372
octopustail <494479914@qq.com>
2020-01-11 13:47:32 +0800

    导出学生记录，用作统计

--- calAE.py
+++ calAE.py
@@ -9,113 +9,129 @@ import pickle
 import datetime
 import time
 import json
+import matplotlib.pyplot as plt
 
-# 校历时间转化成datetime格式，方便后续将记录划归到学期
-l = ['2009-08-31','2010-03-01','2010-08-31','2011-02-21','2011-08-29','2012-02-20','2012-08-31']
-# schoolCalender = map(lambda x: datetime.datetime.strptime(x,'%Y-%m-%d').date(), l)
-li = []
-for i in l:
-    li.append(datetime.datetime.strptime(i,'%Y-%m-%d'))
 
-def find_semester(date):
-    r = range(0, len(li)-1)
-    res = 7
-    for i in r:
-        if (date >= li[i]) & (date < li[i+1]):
-            res = i+1
-            break
-        if(date >= li[6]) | (date<li[1]):
-            break
-    return res
+# 零点时间
+STAN = datetime.datetime.strptime("00:00:00", "%H:%M:%S")
+# 时间区间是10分钟
+TIME_PIECE = 10 * 60
+
+
 # 计算真实熵
+def contains(small, big):
+    for i in range(len(big)-len(small)+1):
+        if big[i:i+len(small)] == small:
+            return True
+    return False
+def actual_entropy(l):
+    n = len(l)
+    sequence = [l[0]]
+    sum_gamma = 0
 
-print("++++++++++++++ Loading Data ++++++++++++++++")
-data = pd.read_csv("/Volumes/Mac/eduData_octo/record_ae_lib/data/comsumption_10.csv", header=None, sep=',')
-
-print("-------------- Dropping Data --------------")
-# column：0   1    2     3
-# 学号  刷卡日期   刷卡时间  刷卡地点
-data = data.head(5000)
-record_df = data.drop([4, 5, 6], axis=1)
-record_df.columns = ["sid","s_date","s_time","s_location"]
-
-# 学生的行为序列（按校历学期划分）
-stu_sequence_dict = {}
-
-print("++++++++++++++ Filling stu_sequence_dict ++++++++++++++++")
-length = record_df.shape[0]
-limit = range(1, 10000)
-for i, r in record_df.iterrows():
-# for r in limit:
-    # 如果stu_sequence_dict中还没有添加该条学生的信息，则在stu_sequence_dict中为该学生初始化
-    if r["sid"] not in stu_sequence_dict.keys():
-        {"sems1":[], "sems2":[], "sems3":[], "sems4":[], "sems5":[], "sems6":[]}
-        stu_sequence_dict[r["sid"]] = {}
-        stu_sequence_dict[r["sid"]]["food"] = {"sems1":[], "sems2":[], "sems3":[], "sems4":[], "sems5":[], "sems6":[]}
-        stu_sequence_dict[r["sid"]]["shower"] = {"sems1":[], "sems2":[], "sems3":[], "sems4":[], "sems5":[], "sems6":[]}
-        stu_sequence_dict[r["sid"]]["hotwater"] = {"sems1":[], "sems2":[], "sems3":[], "sems4":[], "sems5":[], "sems6":[]}
-        stu_sequence_dict[r["sid"]]["library"] = {"sems1":[], "sems2":[], "sems3":[], "sems4":[], "sems5":[], "sems6":[]}
-    d = datetime.datetime.strptime(r["s_date"]+' '+r["s_time"], '%Y-%m-%d %H:%M:%S')
-    sem = find_semester(d)
-    print(sem)
-    if sem >6:
-        continue
-    else:
-        stu_sequence_dict[r["sid"]][r["s_location"]]["sems"+str(sem)].append(d)
-        print(stu_sequence_dict[r["sid"]][r["s_location"]]["sems"+str(sem)])
-    print(i, "/" , length)
-
-print(stu_sequence_dict)
-
-
+    for i in range(1, n):
+        for j in range(i+1, n+1):
+            s = l[i:j]
+            if contains(s, sequence) != True:
+                sum_gamma += len(s)
+                sequence.append(l[i])
+                break
+    print("n", n, "sum_gamma",sum_gamma)
+    ae = (1 / (sum_gamma / n )) * math.log(n)
+    return ae
 #
-# 将数据保存在pkl中
-print('start1....')
-with open('data/sequence.pkl_test', 'wb') as f:
-    pickle.dump(stu_sequence_dict, f)
-print('finish1....')
-
-# print('start2....')
-# print("library_count_df\n",library_count_df)
-# library_count_df.to_pickle('data/library_count_df_10.pkl')
-# print('finish2....')
-#
-# print('start3....')
-# print("shower_count_df\n",shower_count_df)
-# shower_count_df.to_pickle('data/shower_count_df_10.pkl')
-# print('finish3....')
-#
-#
-# print('start4....')
-# print("hotwater_count_df\n",hotwater_count_df)
-# hotwater_count_df.to_pickle('data/hotwater_count_df_10.pkl')
-# print('finish4....')
-#
-#
-
-# def contains(small, big):
-#     for i in range(len(big)-len(small)+1):
-#         if big[i:i+len(small)] == small:
-#             return True
-#     return False
-# def actual_entropy(l):
-#     n = len(l)
-#     sequence = [l[0]]
-#     sum_gamma = 0
-#
-#     for i in range(1, n):
-#         for j in range(i+1, n+1):
-#             s = l[i:j]
-#             if contains(s, sequence) != True:
-#                 sum_gamma += len(s)
-#                 sequence.append(l[i])
-#                 break
-#
-#     ae = 1 / (sum_gamma / n ) * math.log(n)
-#     return ae
-#
+# # 计算可预测性
 # def getPredictability(N, S):
 #     f = lambda x: (((1-x)/(N-1)) **(1-x))* x**x - 2**(-S)
 #     root = mpmath.findroot(f, 1)
 #     return float(root.real)
-# # getPredictability(N, S)
+
+
+def timepicece(x):
+    t = x.split(" ")[1]
+    item = datetime.datetime.strptime(t,"%H:%M:%S")
+    time_piece = math.floor((item-STAN).seconds/TIME_PIECE)
+    return time_piece
+
+ma = lambda x: x.split(" ")
+
+def cal_timepiece(l):
+    if len(l)<3:
+        return
+    nl = np.array(l)
+    sp = np.array([timepicece(xi) for xi in nl]).tolist()
+    ae = actual_entropy(sp)
+    print(ae)
+    return ae
+
+# 读取数据
+print("load data...")
+with open('./data/record_sequence_2_2.json','r') as f:
+    loaded_obj = json.load(f)
+
+food_df = pd.DataFrame(columns=["sems1","sems2","sems3","sems4","sems5","sems6"])
+shower_df = pd.DataFrame(columns=["sems1","sems2","sems3","sems4","sems5","sems6"])
+hotwater_df = pd.DataFrame(columns=["sems1","sems2","sems3","sems4","sems5","sems6"])
+library_df = pd.DataFrame(columns=["sems1","sems2","sems3","sems4","sems5","sems6"])
+
+print("装载DF")
+for i, k in loaded_obj.items():
+    f = k["food"]
+    s = k["shower"]
+    h = k["hotwater"]
+    l = k["library"]
+    # print(f["sems1"],f["sems2"],f["sems3"],f["sems4"],f["sems5"],f["sems6"])
+    food_df.loc[i] = [f["sems1"],f["sems2"],f["sems3"],f["sems4"],f["sems5"],f["sems6"]]
+    shower_df.loc[i] = [s["sems1"],s["sems2"],s["sems3"],s["sems4"],s["sems5"],s["sems6"]]
+    hotwater_df.loc[i] = [h["sems1"],h["sems2"],h["sems3"],h["sems4"],h["sems5"],h["sems6"]]
+    library_df.loc[i] = [l["sems1"],l["sems2"],l["sems3"],l["sems4"],l["sems5"],l["sems6"]]
+
+
+# 刷卡次数统计
+food_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = food_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(lambda x: len(x))
+shower_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = shower_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(lambda x: len(x))
+hotwater_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = hotwater_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(lambda x: len(x))
+library_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = library_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(lambda x: len(x))
+
+# for i,row in food_df.iteritems():
+#     food_df[i] = row.sort_values()
+#     print(food_df[i])
+# print(food_df)
+# # 画个刷卡次数统计图
+# food_df[["sems1","sems2"]].plot()
+# plt.show()
+
+
+
+
+
+# mapapply 计算每个学期的真实熵
+# food_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = food_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(cal_timepiece)
+# shower_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = shower_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(cal_timepiece)
+# hotwater_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = hotwater_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(cal_timepiece)
+# library_df[["sems1","sems2","sems3","sems4","sems5","sems6"]]  = library_df[["sems1","sems2","sems3","sems4","sems5","sems6"]].applymap(cal_timepiece)
+
+
+# print('start4....')
+# food_df.to_pickle('data/new_food_count_10.pkl')
+# print('finish4....')
+
+print('start1....')
+print(shower_df)
+shower_df.to_pickle('data/new_shower_count_10.pkl')
+print('finish1....')
+
+print('start2....')
+print(hotwater_df)
+hotwater_df.to_pickle('data/new_hotwater_count_10.pkl')
+print('finish2....')
+#
+print('start3....')
+print(library_df)
+library_df.to_pickle('data/new_library_count_10.pkl')
+print('finish3....')
+#
+print('start4....')
+print(food_df)
+food_df.to_pickle('data/new_food_count_10.pkl')
+print('finish4....')
